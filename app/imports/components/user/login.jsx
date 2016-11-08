0ab93e32eb4351cfb4ebeeadc1d8a5893fe8b403
cas <cas@interglobal.vision>
2016-11-07 18:59:04 -0600

    add login component

--- app/imports/components/user/login.jsx
+++ app/imports/components/user/login.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
+import { Accounts } from '/imports/components/accounts.jsx';
+
 export class UserLogin extends Component {
   render() {
     return(
       <div className="row">
         <section className="fluid-col s-12 m-9">
           <h3>Login</h3>
-          >>> form goes here
+          <Accounts.ui.LoginForm />
         </section>
       </div>
     );
