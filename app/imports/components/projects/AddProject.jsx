dc9e2aa685b73cd5dafc2711a995db4f895fef06
cas <cas@interglobal.vision>
2016-11-16 17:21:39 -0600

    add scaffold project form (controlled)

--- app/imports/components/projects/AddProject.jsx
+++ app/imports/components/projects/AddProject.jsx
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const AddProject = () => {
-  return 'holi';
+import { ProjectForm } from '/imports/components/projects/ProjectForm.jsx';
+import { SingleInput } from '/imports/components/inputs/SingleInput.jsx';
+
+export class AddProject extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      url: 'http://',
+    };
+
+    this.onSubmitHandle = this.onSubmitHandle.bind(this);
+    this.onNameChange = this.onNameChange.bind(this);
+    this.onUrlChange = this.onUrlChange.bind(this);
+  }
+
+  onNameChange(e) {
+    this.setState({
+      name: e.target.value,
+    });
+  }
+
+  onUrlChange(e) {
+    this.setState({
+      url: e.target.value,
+    });
+  }
+
+  onSubmitHandle(event) {
+    event.preventDefault();
+
+    const project = {
+      name: this.state.name,
+      url: this.state.url,
+    };
+
+    console.log(project);
+  }
+
+  render() {
+    return(
+      <form onSubmit={this.onSubmitHandle}>
+        <SingleInput  inputType='text' title='Project Name' name='project-name-input' controlFunc={this.onNameChange} content={this.state.name} placeholder='Project name/title' />
+        <SingleInput  inputType='text' title='URL' name='project-url-input' controlFunc={this.onUrlChange} content={this.state.url} placeholder='URL' />
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 };
-
-export default AddProject;
