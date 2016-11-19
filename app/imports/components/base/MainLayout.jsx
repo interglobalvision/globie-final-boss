55249df8b28f715b58ad47120d67b8b798bc0159
cas <cas@interglobal.vision>
2016-11-18 19:58:37 -0600

    add blueprint css

--- app/imports/components/base/MainLayout.jsx
+++ app/imports/components/base/MainLayout.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '/node_modules/@blueprintjs/core/dist/blueprint.css';
 
 // import { Header } from '/imports/components/header.jsx';
 // import { Footer } from '/imports/components/footer.jsx';
