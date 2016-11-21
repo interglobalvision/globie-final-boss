87a3cc8f9e078913ac5e534291c6d2ede0413f2e
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2016-11-21 17:24:26 -0600

    add flex-grid classes. add inputs to form

--- app/imports/components/projects/AddProject.jsx
+++ app/imports/components/projects/AddProject.jsx
@@ -56,15 +56,67 @@ export class AddProject extends Component {
   render() {
     return(
       <form onSubmit={this.onSubmitHandle}>
-        <label className='pt-label pt-inline'>
-          Name
-          <input className='pt-input' type='text' name='project-name-input' onChange={this.onNameChange} value={this.state.name} placeholder='Project name/title' />
-        </label>
-        <label className='pt-label pt-inline'>
-          URL
-          <input className='pt-input' type='text' name='project-url-input' onChange={this.onUrlChange} value={this.state.url} placeholder='URL' />
-        </label>
-        <button type='submit' className='pt-button pt-large'>Add Project</button>
+
+        <div className='grid-row'>
+          <div className='grid-item item-s-12 item-m-4'>
+            <label className='pt-label pt-inline'>
+              Title
+              <input className='pt-input' type='text' name='project-name-input' onChange={this.onNameChange} value={this.state.name} placeholder='Project title' />
+            </label>
+          </div>
+          <div className='grid-item item-s-12 item-m-4'>
+            <label className='pt-label pt-inline'>
+              URL
+              <input className='pt-input' type='text' name='project-url-input' onChange={this.onUrlChange} value={this.state.url} placeholder='URL' />
+            </label>
+          </div>
+        </div>
+
+        <div className='grid-row'>
+          <div className='grid-item item-s-12 item-m-4'>
+            <label className='pt-label pt-inline'>
+              Client
+              <input className='pt-input' type='text' name='project-client-input' onChange={this.onClientChange} value={this.state.client} placeholder='Client' />
+            </label>
+          </div>
+          <div className='grid-item item-s-12 item-m-4'>
+            <button className='pt-button pt-large'>Add New Client</button>
+          </div>
+        </div>
+
+        <div className='grid-row'>
+          <div className='grid-item item-s-12 item-m-4'>
+            <label className='pt-label pt-inline'>
+              Country
+              <input className='pt-input' type='text' name='project-country-input' onChange={this.onCountryChange} value={this.state.country} placeholder='Country' />
+            </label>
+          </div>
+        </div>
+
+        <div className='grid-row'>
+          <div className='grid-item item-s-12 item-m-4'>
+            <label className='pt-label pt-inline'>
+              Quote time
+              <input className='pt-input' type='text' name='project-time-input' onChange={this.onTimeChange} value={this.state.time} placeholder='Time' />
+            </label>
+          </div>
+          <div className='grid-item item-s-12 item-m-4'>
+            <label className='pt-label pt-inline'>
+              Quote rate
+              <input className='pt-input' type='text' name='project-rate-input' onChange={this.onRateChange} value={this.state.rate} placeholder='Rate' />
+            </label>
+          </div>
+          <div className='grid-item item-s-12 item-m-4'>
+            <label className='pt-label pt-inline'>
+              Currency
+              <input className='pt-input' type='text' name='project-currency-input' onChange={this.onCurrencyChange} value={this.state.currency} placeholder='Currency' />
+            </label>
+          </div>
+        </div>
+
+        <div className='grid-row'>
+          <button type='submit' className='pt-button pt-large'>Add Project</button>
+        </div>
       </form>
     );
   }
