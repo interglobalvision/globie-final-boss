235bbe1791922c8b7baf3c77a6fa26cbc8c97a8a
cas <cas@interglobal.vision>
2016-11-25 19:54:24 -0600

    add user role checks on addProject() method

--- app/imports/api/projectsMethods.js
+++ app/imports/api/projectsMethods.js
@@ -35,7 +35,17 @@ export const addProject = new ValidatedMethod({
   }).validator(),
 
   run({name, url, client, minDays, maxDays, rate, currency, minQuote, maxQuote}) {
-    // TODO user role check
+
+    // Check if user is logged in
+    if (!this.userId) {
+      throw new Meteor.Error('Projects.methods.insert.not-logged-in', 'Must be logged in to create an application.');
+    }
+
+    // Check if user is admin
+    if (!Roles.userIsInRole(this.userId, 'admin')) {
+      throw new Meteor.Error('Projects.methods.insert.not-allowed', 'Must be admin to do this.');
+    }
+
     Projects.insert({
       name,
       url,
