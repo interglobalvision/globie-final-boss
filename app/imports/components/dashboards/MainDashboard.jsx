7608aaeeaaad41f5eaaf5c93ef58b7011a6d6234
Patrick Best <patrickbest@patrickbest.com>
2017-03-14 17:31:40 -0600

    scaffold main dashboard component

--- app/imports/components/dashboards/MainDashboard.jsx
+++ app/imports/components/dashboards/MainDashboard.jsx
@@ -1,13 +1,59 @@
 import React, { Component } from 'react';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
+import { ListProjects } from '/imports/components/projects/ListProjects.jsx';
 
 export class MainDashboard extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  greeting() {
+    const greetings = [
+      'Tungjatjeta',
+      'Hi',
+      'Degemer mad',
+      'Hola',
+      'Hello',
+      'Holi',
+      'Bok',
+      'Hej',
+      'Selam',
+      'Namaskaar',
+      'Doumo',
+      'Ahn nyeong',
+      'Sat sri akal',
+      'Xin chào',
+      'Kóyo',
+      'Silaw',
+      'Um waynuma?',
+      'Nano toka',
+      'Saluton',
+    ];
+
+    const user = Session.get('User');
+
+    return greetings[_.random(0, greetings.length)] + ' ' + user.emails[0].address;
+  }
+
   render() {
     return(
-      <div className="grid-row">
-        <section className="grid-item item-s-12">
-          <h1>Dashboard</h1>
-        </section>
-      </div>
+      <section id="dashboard-main">
+        <div className="grid-row">
+          <section className="grid-item item-s-12">
+            <h3>{this.greeting()}</h3>
+          </section>
+        </div>
+        <ListProjects />
+        <div className="grid-row">
+          <section className="grid-item item-s-12">
+            <h3>Actions</h3>
+            <ul>
+              <li><a href={FlowRouter.path('addProject')} className="button">Add Project</a></li>
+            </ul>
+          </section>
+        </div>
+      </section>
     );
   }
 };
---
+++ app/imports/components/projects/ListProjects.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export class ListProjects extends Component {
+
+  render() {
+    return(
+      <div className="grid-row">
+        <section className="grid-item item-s-12">
+          <h3>*PROJECT LIST COMPONENT*</h3>
+        </section>
+      </div>
+    )
+  }
+
+}
