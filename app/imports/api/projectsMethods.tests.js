a518b11ae2b9f77e849460850555a51cb1703fb4
cas <cas@interglobal.vision>
2017-03-14 19:56:35 -0600

    add not-logged-user test for Projects.methods.add

--- app/imports/api/projectsMethods.tests.js
+++ app/imports/api/projectsMethods.tests.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Random } from 'meteor/random';
-import { assert } from 'meteor/practicalmeteor:chai';
+import { assert, expect } from 'meteor/practicalmeteor:chai';
 import { Accounts } from 'meteor/accounts-base';
 
 import { Projects } from '/imports/collections/projects.js';
@@ -9,14 +9,16 @@ import { addProject } from '/imports/api/projectsMethods.js';
 if (Meteor.isServer) {
   describe('Projects', () => {
     describe('methods', () => {
-      it('admin user can add new project', () => {
 
+      beforeEach(() => {
         // Delete all Projects
         Projects.remove({});
 
         // Delete all users
         Meteor.users.remove({});
+      });
 
+      it('admin user can add new project', () => {
         // Create admin user
         const userId = Accounts.createUser({
           username: 'testAdmin',
@@ -51,6 +53,29 @@ if (Meteor.isServer) {
         assert.equal(Projects.find({
           name: 'Project Name',
         }).count(), 1);
+
+      });
+
+      it('not logged user can\'t add new project', () => {
+        // Find the internal implementation of the Projects add method so we can
+        // test it in isolation
+        const addProject = Meteor.server.method_handlers['Projects.methods.add'];
+
+        // Set up fake project data
+        const newProject = {
+          name: 'Project Name',
+          url: 'http://project.com',
+          client: 'Client Name',
+          minDays: 1,
+          maxDays: 2,
+          rate: 4000,
+          currency: 'MXN',
+          minQuote: 4000,
+          maxQuote: 8000,
+        };
+
+        // Verify that the method does what we expected
+        expect(addProject.apply.bind({}, [newProject])).to.throw(Error);
 
       });
     });
