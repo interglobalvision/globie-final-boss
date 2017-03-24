9c25046efb9d9393b818fd1b1467642f1b6c2170
Patrick Best <patrickbest@patrickbest.com>
2017-03-24 14:35:21 -0600

    move admin dashboard to /dashboard/admin

--- app/imports/components/dashboards/AdminDashboard.jsx
+++ app/imports/components/dashboards/AdminDashboard.jsx
@@ -3,16 +3,19 @@ import React, { Component } from 'react';
 import { ListWorkersContainer } from '/imports/containers/ListWorkersContainer.jsx';
 import { AddWorker } from '/imports/components/workers/AddWorker.jsx';
 
-export class MainDashboard extends Component {
+export class AdminDashboard extends Component {
   render() {
     return(
       <section id="dashboard">
         <div className="grid-row">
           <div className="grid-item item-s-12">
-            <h1>Dashboard</h1>
+            <h1>Admin Dashboard</h1>
           </div>
         </div>
 
+        <ListWorkersContainer />
+
+        <AddWorker />
       </section>
     );
   }
