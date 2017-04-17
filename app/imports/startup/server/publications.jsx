8b33360b86a17926d97ef4d51bc0d4ff432cf26f
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-04-17 18:52:47 -0500

    publish customers

--- app/imports/startup/server/publications.jsx
+++ app/imports/startup/server/publications.jsx
@@ -1,11 +1,23 @@
 import { Meteor } from 'meteor/meteor';
 
 import { Projects } from '/imports/collections/projects.js';
+import { Customers } from '/imports/collections/customers.js';
 
 Meteor.publish('dashboard.projects.all', function() {
 
   if (Roles.userIsInRole(this.userId, ['worker', 'admin'])) {
     return Projects.find({});
+  } else {
+    this.stop();
+    return;
+  }
+
+});
+
+Meteor.publish('dashboard.customers.all', function() {
+
+  if (Roles.userIsInRole(this.userId, ['worker', 'admin'])) {
+    return Customers.find({});
   } else {
     this.stop();
     return;
