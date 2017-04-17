83ec5036308ab194570da0cedbb0036cc8d519dd
Michael Ray-Von <mikey@Michaels-MacBook-Pro.local>
2017-04-17 18:52:18 -0500

    add addCustomer method

---
+++ app/imports/api/customersMethods.js
@@ -0,0 +1,26 @@
+import { Meteor } from 'meteor/meteor'
+
+import { Customers } from '/imports/collections/customers.js';
+import { CustomerSchema } from '/imports/schemas/CustomerSchema.js';
+
+export const addCustomer = new ValidatedMethod({
+  name: 'Customers.methods.add',
+  validate: CustomerSchema.validator(),
+
+  run({name}) {
+
+    // Check if user is logged in
+    if (!this.userId) {
+      throw new Meteor.Error('Customers.methods.insert.not-logged-in', 'Must be logged in to create an application.');
+    }
+
+    // Check if user is admin
+    if (!Roles.userIsInRole(this.userId, 'admin')) {
+      throw new Meteor.Error('Customers.methods.insert.not-allowed', 'Must be admin to do this.');
+    }
+
+    Customers.insert({
+      name,
+    });
+  }
+});
