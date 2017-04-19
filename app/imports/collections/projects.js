56c3593bce00b95ec0d1ce7814da5b458e410f35
cas <cas@interglobal.vision>
2017-04-18 19:07:23 -0500

    fix indentation in collections

--- app/imports/collections/projects.js
+++ app/imports/collections/projects.js
@@ -1,31 +1,31 @@
 import { Meteor } from 'meteor/meteor';
 
 class ProjectsCollection extends Mongo.Collection {
-	insert(doc, callback) {
-		doc.createdAt = doc.createdAt || new Date();
-		doc.updatedAt = doc.updatedAt || new Date();
+  insert(doc, callback) {
+    doc.createdAt = doc.createdAt || new Date();
+    doc.updatedAt = doc.updatedAt || new Date();
 
-		return super.insert(doc, callback);
-	}
+    return super.insert(doc, callback);
+  }
 
-	update(selector, modifier, ...optionsAndCallback) {
+  update(selector, modifier, ...optionsAndCallback) {
 
-		// When modifying whole document (ex. Mongol)
-		if(_.isUndefined(modifier.$set)) {
-			modifier['updatedAt'] = modifier['updatedAt'] || new Date();
-		} else if (!_.isUndefined(modifier.$set)) {
-		// When modifying whole document (ex. Mongol)
-			modifier.$set['updatedAt'] = modifier.$set['updatedAt'] || new Date();
-		}
+    // When modifying whole document (ex. Mongol)
+    if(_.isUndefined(modifier.$set)) {
+      modifier['updatedAt'] = modifier['updatedAt'] || new Date();
+    } else if (!_.isUndefined(modifier.$set)) {
+      // When modifying whole document (ex. Mongol)
+      modifier.$set['updatedAt'] = modifier.$set['updatedAt'] || new Date();
+    }
 
-		return super.update(selector, modifier, ...optionsAndCallback);
-	}
+    return super.update(selector, modifier, ...optionsAndCallback);
+  }
 }
 
 export const Projects = new ProjectsCollection('Projects');
 
 Projects.deny({
-	insert() { return true; },
-	update() { return true; },
-	remove() { return true; },
+  insert() { return true; },
+  update() { return true; },
+  remove() { return true; },
 });
